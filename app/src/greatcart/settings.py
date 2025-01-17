985726f098931f31f5af6d201d30b2156ffe0dad
Tran Xuan Tu <xtutran@users.noreply.github.com>
2025-01-18 00:02:32 +0800

    Feature: enalbe production mode (#5)

--- app/src/greatcart/settings.py
+++ app/src/greatcart/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "greatcart.urls"
@@ -127,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     "greatcart/static",
