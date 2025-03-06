1ef4fb200d817399ddb0b6b9775b3d029c54369e
jqadan <jqadan@gmail.com>
2025-03-06 16:02:53 +1100

    Update urls.py

--- app/src/accounts/urls.py
+++ app/src/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from . import views
+from .views import google_auth_callback
+
 
 urlpatterns = [
     # path("heartbeat", views.heart_beat, name="heartbeat"),
     path("register/", views.register, name="register"),
+    path('auth/google/callback/', google_auth_callback, name='google_auth_callback'),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     # path("dashboard/", views.dashboard, name="dashboard"),
